3fd47d18c7b3909a823bb16181adb8c66c6ba3e4
Артем Лобов <artemlobov@192.168.1.8>
2023-02-28 19:22:04 +0300

    header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,18 +1,21 @@
 import classes from "./Header.module.css";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Header = function () {
-
-  return (
-    <header className={classes.header}>
-      <a className={classes.link}>Dashboard</a>
-      <img className={classes.logo} src="/assets/logo.svg"></img>
-      <img className={classes.lang} src="/assets/lang.svg"></img>
-    </header>
-  )
+    return (
+        <header className={classes.header}>
+            <a className={classes.link}>Dashboard</a>
+            <img className={classes.logo} src="/assets/logo.svg"></img>
+            <img className={classes.lang} src="/assets/lang.svg"></img>
+            <div id="burger-menu" onClick={() => {
+                document.getElementById("burger-menu")?.classList.toggle(classes.menu_open)
+            }} className={classes.menu}>
+                <img className={classes.menu_icon} src="/assets/menu.svg"></img>
+                <a className={classes.link}>Dashboard</a>
+            </div>
+        </header>
+    )
 }
 
 export default Header;
-
-
-
