969ac9356e7b65dc56ad2c4253ceb576aac01f14
Артем Лобов <artemlobov@192.168.1.8>
2023-02-28 21:29:44 +0300

    header v2

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -6,15 +6,19 @@ const Header = function () {
     return (
         <header className={classes.header}>
             <a className={classes.link}>Dashboard</a>
-            <img className={classes.logo} src="/assets/logo.svg"></img>
+            <img id="logo" className={classes.logo} src="/assets/logo.svg"></img>
             <img className={classes.lang} src="/assets/lang.svg"></img>
             <div id="burger-menu" onClick={() => {
                 document.getElementById("burger-menu")?.classList.toggle(classes.menu_open)
+                document.getElementById("menu-link")?.classList.toggle(classes.menu_link_open)
+                document.getElementById("logo")?.classList.toggle(classes.logo_close)
+                document.getElementById("lang")?.classList.toggle(classes.lang_open)
             }} className={classes.menu}>
                 <img className={classes.menu_icon} src="/assets/menu.svg"></img>
-                <a className={classes.link}>Dashboard</a>
+                <a id="menu-link" className={classes.menu_link}>Dashboard</a>
+                <img id="lang" className={classes.lang} src="/assets/lang.svg"></img>
             </div>
-        </header>
+        </header >
     )
 }
 
