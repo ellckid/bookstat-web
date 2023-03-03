dfc46208d4a8453839708b803b1c225b8ed6fdfd
ellckid <zelyonuy17@gmail.com>
2023-03-03 21:30:09 +0300

    header v4

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,25 +4,36 @@ import { useState } from "react";
 import Lang from "../selector/Lang";
 
 const Header = function () {
-  function openLangInMenu() {
-    document.getElementById("lang")?.classList.remove('lang')
-    document.getElementById("lang")?.classList.toggle(classes.lang_open)
+
+  function openMenu() {
+    document.getElementById("burger-menu")?.classList.toggle(classes.menu_open)
+    document.getElementById("menu-link")?.classList.toggle(classes.menu_link_open)
+    document.getElementById("logo")?.classList.toggle(classes.logo_close)
+    document.getElementById("menu_icon_container")?.classList.toggle(classes.menu_icon_container)
+    document.getElementById("menu_icon")?.classList.toggle(classes.menu_icon_rotate)
+    document.getElementById("menu-lang")?.classList.toggle(classes.lang_open)
   }
   return (
     <header className={classes.header}>
       <a className={classes.link}>Dashboard</a>
       <img id="logo" className={classes.logo} src="/assets/logo.svg"></img>
-      <Lang linkto="/" isRussian={false} func="" />
-      <button id="burger-menu" onClick={() => {
-        document.getElementById("burger-menu")?.classList.toggle(classes.menu_open)
-        document.getElementById("menu-link")?.classList.toggle(classes.menu_link_open)
-        document.getElementById("logo")?.classList.toggle(classes.logo_close)
-        openLangInMenu()
+      <div className={classes.lang_close}>
+        <Lang linkto="/" isRussian={false} />
+      </div>
 
-      }} className={classes.menu}>
-        <img className={classes.menu_icon} src="/assets/menu.svg"></img>
+      <button
+        id="burger-menu"
+        onClick={() => {
+          openMenu()
+        }}
+        className={classes.menu}>
+        <div id="menu_icon_container" >
+          <img id="menu_icon" className={classes.menu_icon} src="/assets/menu.svg"></img>
+        </div>
         <a id="menu-link" className={classes.menu_link}>Dashboard</a>
-        <Lang linkto="/" isRussian={false} func={openLangInMenu} />
+        <div id="menu-lang" className={classes.lang_close}>
+          <Lang linkto="/" isRussian={false} />
+        </div>
       </button >
     </header >
   )
--- src/components/selector/Lang.tsx
+++ src/components/selector/Lang.tsx
@@ -1,25 +1,21 @@
-import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import classes from "./Lang.module.css";
 
 interface langprop {
   linkto: string;
   isRussian: boolean;
-  func: Function | string;
 }
 const Lang = function (prop: langprop) {
-  useEffect(() => {
-    prop.func
-  }, [classes.lang])
+
   return (
     (prop.isRussian)
       ?
-      <Link id="lang" className={classes.lang_link} to={prop.linkto}>
-        <img id="lang" className={classes.lang} src="/assets/rulang.svg"></img>
+      <Link className={classes.lang_link} to={prop.linkto}>
+        <img className={classes.lang} src="/assets/rulang.svg"></img>
       </Link>
       :
-      <Link id="lang" className={classes.lang_link} to={prop.linkto}>
-        <img id="lang" className={classes.lang} src="/assets/englang.svg"></img>
+      <Link className={classes.lang_link} to={prop.linkto}>
+        <img className={classes.lang} src="/assets/englang.svg"></img>
       </Link>
   )
 }
