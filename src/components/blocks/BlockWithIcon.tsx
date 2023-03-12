fd2ebf019aa9cc29a3ae06a18f68da81deb80b4f
ellckid <zelyonuy17@gmail.com>
2023-03-12 19:25:33 +0300

    block with icon fix

--- src/components/blocks/BlockWithIcon.tsx
+++ src/components/blocks/BlockWithIcon.tsx
@@ -10,13 +10,13 @@ const BlockWithIcon = function (prop: Blockprop) {
   return (
     (prop.isIconLeft)
       ?
-      <section className={classes.block_container + " " + classes.right}>
+      <section className={classes.block_container + " " + classes.right} >
         <div className={classes.title_container}>
           <h2 className={classes.h2_title}>{prop.title}</h2>
           <h3 className={classes.h3_title}>{prop.subtitle}</h3>
         </div>
         <img className={classes.block_icon} src={prop.icon_src}></img>
-      </section>
+      </section >
       :
       <section className={classes.block_container + " " + classes.left}>
         <div className={classes.title_container}>
